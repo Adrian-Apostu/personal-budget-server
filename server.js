84772c20545610d69cd4277a97aa691244868c50
Adrian Apostu <apostuadrian6@gmail.com>
2024-07-07 17:18:32 +0300

    Update server.js to serve only API

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+//const path = require('path');
 const app = express();
 
 const allowedOrigins = ['https://personal-budget-tracker-app.netlify.app'];
@@ -23,11 +23,11 @@ app.use(express.json());
 const envelopesRouter = require('./routes/envelopes');
 app.use('/api/envelopes', envelopesRouter);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+//app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', (req, res) => {
+/*app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+});*/
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
