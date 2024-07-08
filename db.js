5c307651cce4a5b827ca01f1328b2ae595335c66
Adrian Apostu <apostuadrian6@gmail.com>
2024-07-08 15:44:30 +0300

    fixing the database deployment

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
-//db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('error', (err, client) => {
-    console.error('Unexpected error on idle client', err.message, err.stack);
+  console.error('Unexpected error on idle client', err.message, err.stack);
 });
 
 module.exports = pool;
