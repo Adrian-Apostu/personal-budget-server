2a735a5bf3b2430ccffe9910a8a9c9ddfa630723
Adrian Apostu <apostuadrian6@gmail.com>
2024-07-10 21:32:30 +0300

    reverting

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
+//db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err.message, err.stack);
+    console.error('Unexpected error on idle client', err.message, err.stack);
 });
 
 module.exports = pool;
